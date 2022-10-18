979fb51215e998776a3262914ccb895b59d0f13e
masraR <100113738+MNAG19@users.noreply.github.com>
2022-10-18 15:11:20 +0700

    muehehehe

---
+++ Kamus/app/src/main/java/com/if5b/mnag/kamus/activities/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.if5b.mnag.kamus.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.if5b.mnag.kamus.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+    private ActivitySplashBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
